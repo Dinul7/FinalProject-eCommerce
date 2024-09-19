32797c8a775d082b48af76cfcdc08f2ba1b12ae0
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-09-19 18:46:50 +0300

    schimbare

---
+++ script.js
@@ -0,0 +1,10 @@
+// const fetchGames = async function () {
+//   const rasp = await fetch(
+//     "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+//   );
+//   const jocuri = await rasp.json();
+
+//   console.log(jocuri);
+// };
+
+// fetchGames();
