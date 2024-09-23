d9f850eba5c5ef851db45a82998136429bb77157
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-09-24 01:26:46 +0300

    mai nimic

--- 01.Home-page/script.js
+++ 01.Home-page/script.js
@@ -1,7 +1,7 @@
 const colWrapper = document.querySelectorAll(".col-wrapper");
 const productCards = document.querySelectorAll(".product-card");
 
-// console.log(productCard.length);
+console.log(productCards);
 // console.log(colWrapper.length);
 const fetchGames = async function () {
   const rasp = await fetch(
@@ -19,41 +19,25 @@ fetchGames();
 
 const printeazaProdus = function (product) {
   console.log(product);
-  // const colWrapper = document.querySelector(".col-wrapper");
-  // const cardElement = document.querySelector(".product-card");
-  // const imgElement = document.createElement("img");
-  // imgElement.src = `${product.thumb}`;
-
-  // const infoElement = document.createElement("div");
-  // infoElement.classList.add("info");
-
-  // const headerThree = document.createElement("h3");
-  // headerThree.innerText = `${product.title}`;
-
-  // const infoP = document.createElement("p");
-  // infoP.innerText = `Sylish cafe chair`;
-  // const priceDiv = document.createElement("div");
-  // priceDiv.classList.add("price");
-
-  // const priceEl = document.createElement("p");
-  // priceEl.innerText = "Rp 2.500.000";
-
-  // priceDiv.appendChild(priceEl);
-
-  // infoElement.appendChild(headerThree);
-  // infoElement.appendChild(infoP);
-  // infoElement.appendChild(priceDiv);
-
-  // cardElement.appendChild(imgElement);
-  // cardElement.appendChild(infoElement);
-  // colWrapper.appendChild(cardElement);
-
-  // productCard.forEach(function (card) {
+  console.log(colWrapper.length);
+  // for (let i = 0; i < colWrapper.length; i++) {
+  //   const card = document.querySelector(".product-card");
   //   console.log(card);
-  // });
-  const productCard = document.querySelectorAll(".product-card");
-  productCard.innerHTML = `
-  <img src="${product.thumb}" />
+
+  //   // <img src="${product.thumb}" />
+  //   //     <div class="info">
+  //   //       <h3>${product.title}</h3>
+  //   //       <p>Sylish cafe chair</p>
+  //   //       <div class="price">
+  //   //         <p>Rp 2.500.000</p>
+  //   //       </div>
+  //   //     </div>
+  //   // `;
+  // }
+  [...productCards].forEach(function (el) {
+    console.log(el);
+    el.innerHTML = `
+    <img src="${product.thumb}" />
       <div class="info">
         <h3>${product.title}</h3>
         <p>Sylish cafe chair</p>
@@ -61,7 +45,8 @@ const printeazaProdus = function (product) {
           <p>Rp 2.500.000</p>
         </div>
       </div>
-  `;
+    `;
+  });
 };
 // productCard.forEach((card) => {
 //   card.innerHTML = `
@@ -80,6 +65,7 @@ const printeazaProdus = function (product) {
 
 function printeazaProduse(arrayDeProduse) {
   for (let i = 0; i < colWrapper.length; i++) {
+    console.log(arrayDeProduse[i]);
     printeazaProdus(arrayDeProduse[i]);
   }
 }
