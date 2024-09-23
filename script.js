af47773ebe2f5c5176d58985b6df79affa1f4dc6
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-09-23 18:39:12 +0300

    putin javascript

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-// const fetchGames = async function () {
-//   const rasp = await fetch(
-//     "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
-//   );
-//   const jocuri = await rasp.json();
+const productCard = document.querySelectorAll(".product-card");
 
-//   console.log(jocuri);
-// };
+const fetchGames = async function () {
+  const rasp = await fetch(
+    "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+  );
+  let jocuri = await rasp.json();
 
-// fetchGames();
+  //   console.log(jocuri);
+
+  displayProducts(jocuri);
+};
+
+fetchGames();
+
+const displayProduct = function (product) {
+  console.log(product);
+  // const productCard = document.querySelectorAll(".product-card");
+  // productCard.forEach((card) => {
+  //   card.innerHTML = `
+  //      <img src="${prod.thumb}" />
+  //            <div class="info">
+  //              <h3>${prod.title}</h3>
+  //              <p>Sylish cafe chair</p>
+  //             <div class="price">
+  //                 <p>Rp 2.500.000</p>
+  //                 <p class="discount">Rp 3.500.000</p>
+  //              </div>
+  //            </div>
+  // `;
+};
+//   const productCard = document.querySelectorAll(".product-card");
+//   productCard.forEach((card) => {
+//     card.innerHTML = `
+//     <img src="${product.thumb}" />
+//                <div class="info">
+//                  <h3>${product.title}</h3>
+//                  <p>Sylish cafe chair</p>
+//                 <div class="price">
+//                     <p>Rp 2.500.000</p>
+//                     <p class="discount">Rp 3.500.000</p>
+//                  </div>
+//                </div>
+//     `;
+
+//   productCard.innerHTML = `
+//   <img src="${product.thumb}" />
+//               <div class="info">
+//                 <h3>${product.title}</h3>
+//                 <p>Sylish cafe chair</p>
+//                <div class="price">
+//                    <p>Rp 2.500.000</p>
+//                    <p class="discount">Rp 3.500.000</p>
+//                 </div>
+//               </div>
+//   `;
+
+function displayProducts(productsArr) {
+  productsArr.forEach((product) => {
+    displayProduct(product);
+    // console.log(product);
+  });
+}
+
+displayProducts(productCard);
