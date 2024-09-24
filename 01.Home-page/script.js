6b685a99b99189fda4956e552de268d55c0de833
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-09-24 11:16:44 +0300

    ss

--- 01.Home-page/script.js
+++ 01.Home-page/script.js
@@ -1,8 +1,8 @@
 const colWrapper = document.querySelectorAll(".col-wrapper");
 const productCards = document.querySelectorAll(".product-card");
+// console.log(productCards.entries());
+const productCardsArr = Array.from(productCards);
 
-console.log(productCards);
-// console.log(colWrapper.length);
 const fetchGames = async function () {
   const rasp = await fetch(
     "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
@@ -18,57 +18,21 @@ const fetchGames = async function () {
 fetchGames();
 
 const printeazaProdus = function (product) {
-  console.log(product);
-  console.log(colWrapper.length);
-  // for (let i = 0; i < colWrapper.length; i++) {
-  //   const card = document.querySelector(".product-card");
-  //   console.log(card);
-
-  //   // <img src="${product.thumb}" />
-  //   //     <div class="info">
-  //   //       <h3>${product.title}</h3>
-  //   //       <p>Sylish cafe chair</p>
-  //   //       <div class="price">
-  //   //         <p>Rp 2.500.000</p>
-  //   //       </div>
-  //   //     </div>
-  //   // `;
-  // }
-  [...productCards].forEach(function (el) {
-    console.log(el);
-    el.innerHTML = `
-    <img src="${product.thumb}" />
-      <div class="info">
-        <h3>${product.title}</h3>
-        <p>Sylish cafe chair</p>
-        <div class="price">
-          <p>Rp 2.500.000</p>
-        </div>
-      </div>
-    `;
-  });
+  const productCardsArr = Array.from(productCards);
+  for (let i = 0; i < productCardsArr.length; i++) {
+    const newItem = productCardsArr[i];
+    console.log(newItem);
+  }
+  console.log(product.title);
 };
-// productCard.forEach((card) => {
-//   card.innerHTML = `
-//    <img src="${product.thumb}" />
-//      <div class="info">
-//        <h3>${product.title}</h3>
-//        <p>Sylish cafe chair</p>
-//        <div class="price">
-//          <p>Rp 2.500.000</p>
-//        </div>
-//      </div>
-//   `;
-// });
-// const printeazaProduse = (arrayDeProduse) =>
-//   arrayDeProduse.forEach((produs) => printeazaProdus(produs));
 
 function printeazaProduse(arrayDeProduse) {
   for (let i = 0; i < colWrapper.length; i++) {
-    console.log(arrayDeProduse[i]);
+    // console.log(arrayDeProduse[i]);
     printeazaProdus(arrayDeProduse[i]);
   }
 }
-// productCards.forEach(function (card) {
-//   printeazaProdus();
-// });
+
+// function printeazaProduse(arrayDeProduse) {
+//   arrayDeProduse.forEach((produs) => printeazaProdus(produs));
+// }
