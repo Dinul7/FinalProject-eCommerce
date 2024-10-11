7b334307e7a15ce6d128d770403364f5086629d0
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-10-11 22:43:03 +0300

    sss

--- 01.Home-page/script.js
+++ 01.Home-page/script.js
@@ -1,11 +1,14 @@
-const colWrapper = document.querySelectorAll(".col-wrapper");
+const prodsWrapper = document.getElementById("prods-wrap");
 const shopBtn = document.querySelector(".cart-logo");
 const shoppingCart = document.querySelector(".shopping-cart");
+const shoppingCartTotal = document.querySelector(".shopping-cart-total");
 const bagBtn = document.querySelector(".fa-shopping-bag");
 const banner = document.querySelector(".banner");
 const clearBtn = document.querySelector(".fa-times-circle");
 const cartProducts = document.querySelector(".shopping-cart-products");
-console.log(clearBtn);
+// const addToCartButton = document.querySelector(".hover-add-to-cart");
+
+// console.log(addToCartButton);
 
 const fetchGames = async function () {
   const rasp = await fetch(
@@ -14,17 +17,50 @@ const fetchGames = async function () {
   let jocuri = await rasp.json();
 
   const jocuriArr = jocuri;
-  console.log(jocuriArr.length);
-  console.log(jocuriArr);
+  // console.log(jocuriArr.length);
+  // console.log(jocuriArr);
   printeazaProduse(jocuriArr);
 };
 
 fetchGames();
 
 const printeazaProdus = function (product) {
-  for (i = 0; i < colWrapper.length; i++) {
+  // console.log(product);
+
+  for (i = 0; i < product.length; i++) {
+    const colWrapper = document.createElement("div");
+    const hoverDiv = document.createElement("div");
     const prodDiv = document.createElement("div");
+    colWrapper.classList.add("col-wrapper");
     prodDiv.classList.add("product-card");
+    hoverDiv.classList.add("hover-wrapper");
+
+    prodsWrapper.appendChild(colWrapper);
+
+    // console.log(addHover);
+
+    hoverDiv.innerHTML = `
+     <div class="wrap">
+      <div class="wrapper-box">
+     <button class="hover-add-to-cart">Add To cart</button>
+     <div class="hover-actions">
+       <div class="icons">
+         <i class="fa fa-share-alt"></i>
+         <p>Share</p>
+       </div>
+       <div class="icons">
+         <i class="fa fa-balance-scale"></i>
+         <p>Compare</p>
+       </div>
+       <div class="icons">
+         <i class="fa fa-heart-o"></i>
+         <p>Like</p>
+       </div>
+     </div>
+   </div>
+ </div>
+    
+    `;
 
     prodDiv.innerHTML = `
     <img src="${product[i].thumb}" />
@@ -45,17 +81,38 @@ const printeazaProdus = function (product) {
     
     `;
 
-    colWrapper[i].appendChild(prodDiv);
+    colWrapper.appendChild(hoverDiv);
+    colWrapper.appendChild(prodDiv);
+    // const addHover = document.querySelectorAll(".hover-add-to-cart");
+
+    // const prodsWrapperLength = prodsWrapper.childElementCount;
+    // addHover.forEach(function (el) {
+    //   el.addEventListener("click", function () {
+    //     for (let i = 0; i < prodsWrapperLength; i++) {
+    //       addToCart(product[i].internalName);
+    //       console.log(productsCart);
+    //     }
+    //   });
+    // });
   }
 };
 
 function printeazaProduse(arrayDeProduse) {
   const produsePentruCard = arrayDeProduse.slice(0, 8);
-  console.log(produsePentruCard);
+  // console.log(produsePentruCard);
 
   printeazaProdus(produsePentruCard);
 }
 
+const productsCart = [];
+
+function addToCart(product) {
+  productsCart.push(product);
+}
+
+// console.log(productsCart);
+
+// Event listeners
 shopBtn.addEventListener("click", () => {
   shoppingCart.classList.remove("no-show");
 });
@@ -66,7 +123,9 @@ bagBtn.addEventListener("click", () => {
 
 clearBtn.addEventListener("click", () => {
   cartProducts.innerHTML = "";
-  clearBtn.style.display = "none";
+  clearBtn.style.visibility = "hidden";
+
+  shoppingCartTotal.innerHTML = "";
 });
 
 const chooseHighScore = (steamScore, metacriticScore) =>
@@ -74,3 +133,10 @@ const chooseHighScore = (steamScore, metacriticScore) =>
 
 const chosePlatform = (steam, metaCritic) =>
   steam > metaCritic ? "Steam" : "MetaCritic";
+
+//
+// const addToCartButton = document.querySelector(".hover-add-to-cart");
+
+// addToCartButton.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
