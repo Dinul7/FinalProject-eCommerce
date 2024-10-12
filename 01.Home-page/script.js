303f9e5a179e01f57e0a1d92ae05153feeb4e747
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-10-12 07:38:47 +0300

    add to cart done

--- 01.Home-page/script.js
+++ 01.Home-page/script.js
@@ -25,83 +25,78 @@ const fetchGames = async function () {
 fetchGames();
 
 const printeazaProdus = function (product) {
-  // console.log(product);
+  const colWrapper = document.createElement("div");
+  const prodDiv = document.createElement("div");
+  const hoverDiv = document.createElement("div");
 
-  for (i = 0; i < product.length; i++) {
-    const colWrapper = document.createElement("div");
-    const hoverDiv = document.createElement("div");
-    const prodDiv = document.createElement("div");
-    colWrapper.classList.add("col-wrapper");
-    prodDiv.classList.add("product-card");
-    hoverDiv.classList.add("hover-wrapper");
+  // addButton.classList.add("hover-add-to-cart");
+  const wrapDiv = document.createElement("div");
+  const wrappedBoxDiv = document.createElement("div");
+  const addButton = document.createElement("button");
+  const hoverActions = document.createElement("div");
 
-    prodsWrapper.appendChild(colWrapper);
+  colWrapper.classList.add("col-wrapper");
+  prodDiv.classList.add("product-card");
+  hoverDiv.classList.add("hover-wrapper");
+  wrapDiv.classList.add("wrap");
+  wrappedBoxDiv.classList.add("wrapper-box");
+  addButton.classList.add("hover-add-to-cart");
+  hoverActions.classList.add("hover-actions");
+  addButton.innerText = "Add to Cart";
+  hoverActions.innerHTML = `
+  <div class="icons">
+            <i class="fa fa-share-alt"></i>
+            <p>Share</p>
+          </div>
+          <div class="icons">
+            <i class="fa fa-balance-scale"></i>
+            <p>Compare</p>
+          </div>
+          <div class="icons">
+            <i class="fa fa-heart-o"></i>
+            <p>Like</p>
+          </div>
+  `;
 
-    // console.log(addHover);
-
-    hoverDiv.innerHTML = `
-     <div class="wrap">
-      <div class="wrapper-box">
-     <button class="hover-add-to-cart">Add To cart</button>
-     <div class="hover-actions">
-       <div class="icons">
-         <i class="fa fa-share-alt"></i>
-         <p>Share</p>
-       </div>
-       <div class="icons">
-         <i class="fa fa-balance-scale"></i>
-         <p>Compare</p>
-       </div>
-       <div class="icons">
-         <i class="fa fa-heart-o"></i>
-         <p>Like</p>
-       </div>
-     </div>
-   </div>
- </div>
-    
-    `;
-
-    prodDiv.innerHTML = `
-    <img src="${product[i].thumb}" />
-              <div class="info">
-                <h3>${product[i].title}</h3>
-                <p>${chosePlatform(
-                  product[i].steamRatingPercent,
-                  product[i].metacriticScore
-                )} score: ${chooseHighScore(
-      product[i].steamRatingPercent,
-      product[i].metacriticScore
-    )}</p>
-                <div class="price">
-                  <p>${product[i].salePrice}$</p>
-                  <p class="discount">${product[i].normalPrice}$</p>
+  prodDiv.innerHTML = `
+      <img src="${product.thumb}" />
+                <div class="info">
+                  <h3>${product.title}</h3>
+                  <p>${chosePlatform(
+                    product.steamRatingPercent,
+                    product.metacriticScore
+                  )} score: ${chooseHighScore(
+    product.steamRatingPercent,
+    product.metacriticScore
+  )}</p>
+                  <div class="price">
+                    <p>${product.salePrice}$</p>
+                    <p class="discount">${product.normalPrice}$</p>
+                  </div>
                 </div>
-              </div>
-    
-    `;
 
-    colWrapper.appendChild(hoverDiv);
-    colWrapper.appendChild(prodDiv);
-    // const addHover = document.querySelectorAll(".hover-add-to-cart");
+      `;
 
-    // const prodsWrapperLength = prodsWrapper.childElementCount;
-    // addHover.forEach(function (el) {
-    //   el.addEventListener("click", function () {
-    //     for (let i = 0; i < prodsWrapperLength; i++) {
-    //       addToCart(product[i].internalName);
-    //       console.log(productsCart);
-    //     }
-    //   });
-    // });
-  }
+  colWrapper.appendChild(hoverDiv);
+  hoverDiv.appendChild(wrapDiv);
+  wrapDiv.appendChild(wrappedBoxDiv);
+  wrappedBoxDiv.appendChild(addButton);
+  wrappedBoxDiv.appendChild(hoverActions);
+  colWrapper.appendChild(prodDiv);
+  prodsWrapper.appendChild(colWrapper);
+
+  addButton.addEventListener("click", function () {
+    addToCart(product);
+    console.log(productsCart);
+  });
 };
 
 function printeazaProduse(arrayDeProduse) {
   const produsePentruCard = arrayDeProduse.slice(0, 8);
-  // console.log(produsePentruCard);
-
-  printeazaProdus(produsePentruCard);
+  console.log(produsePentruCard);
+  produsePentruCard.forEach(function (el) {
+    printeazaProdus(el);
+  });
 }
 
 const productsCart = [];
